67d06f472cbafa0c02f72c228f748afeec2c17be
Sylvain Chabert <schabert@arkoon.dev>
2014-12-10 17:41:29 +0100

    Add tasks for test and autotest

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,37 @@ module.exports = function(grunt) {
 
         // configuration des tâches grunt
 
+        mochaTest: {
+            unit: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/{,**}/*.js']
+        }},
         mocha_istanbul: {
             coverage: {
                 src: 'test'
             }
-        }
+        },
+        watch: {
+            test: {
+                files: [
+                    'src/{,**}/*.js',
+                    'test/{,**}/*.js'
+                ],
+                tasks: ['test']
+            }
+				}
     });
 
-    grunt.loadNpmTasks('grunt-mocha-istanbul')
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Les tâches sont enregistrées ici
     grunt.registerTask('default', []);
 
+	  grunt.registerTask('test', ['mochaTest:unit']);
     grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
+    grunt.registerTask('autotest', ['watch:test']);
 };
--- README.md
+++ README.md
@@ -3,4 +3,17 @@ Sample Grunt Mocha TDD
 
 This is a sample project to do TDD (in coding dojo for example) with a javascript project, using grunt and mocha.
 
-There is a coverage task to generate code coverage of tests.
+To launch tests:
+```
+grunt test
+```
+
+To launch tests automatically after source ou test file modification:
+```
+grunt autotest
+```
+
+To launch tests with coverage:
+```
+grunt coverage
+```
